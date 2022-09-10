abff05e55ca80254610b72bd8dd74c49fbc3c546
Mark <mutofifin@mail.ru>
2022-09-10 18:28:55 +0300

    36 задание

---
+++ EXS36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+void RandomArray(int[] a){
+	Random random = new Random();
+	for(int i = 0; i < a.Length; i++) a[i] = random.Next(1, 100);
+	Console.WriteLine(String.Join(", ", a));
+}
+
+int Summ(int[] a){
+	int summ = 0;
+	for(int i = 1; i < a.Length; i += 2){
+		summ += a[i];
+	}
+	return summ;
+}
+
+
+
+int[] array = new int[10];
+RandomArray(array);
+Console.WriteLine("Сумму элементов, стоящих на нечётных позициях массива равна: " + Summ(array));
